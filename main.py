c31086efc0626d27e67d8b93b050658fce834bd2
WildBlueJay <gerstemeierj@gmail.com>
2025-01-19 23:54:44 -0600

    G: Initialized a grouping methodology for updatable an drawable objects

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 import constants
 from constants import *
 import player
@@ -9,23 +8,24 @@ def main():
     pygame.init()
 
     #Required print for the assignment
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    #print("Starting asteroids!")
+    #print(f"Screen width: {constants.SCREEN_WIDTH}")
+    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
     gametime = pygame.time.Clock()
     dt = 0
-    print(dt)
+    #DB print(dt)
 
     #Setup and Init the Game Screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #adding the player
+
+    #adding the player and initiating groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player1 = player.Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     
     while True:
-        # Calculate dt every frame
-        dt = gametime.tick(60) / 1000
-
         # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,11 +35,17 @@ def main():
         screen.fill("black")
         
         # Update and draw the player
-        player1.update(dt)
-        player1.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
 
         # Refresh the display
         pygame.display.flip()
+
+        # Limit the FPS to 60
+        dt = gametime.tick(60) / 1000
+        #DB print(dt)
 
 if __name__ == "__main__":
     main()
