ce7f8403c9ba4869aea646404e2c9a4d93080338
WildBlueJay <gerstemeierj@gmail.com>
2025-01-23 13:52:58 -0600

    Overhaul for adding shot.py and handling shots

--- main.py
+++ main.py
@@ -4,60 +4,52 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
-
-    # Required print for the assignment
-    #print("Starting asteroids!")
-    #print(f"Screen width: {constants.SCREEN_WIDTH}")
-    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
-
-    gametime = pygame.time.Clock()
-    dt = 0
-    #DB print(dt)
-
-    # Setup and Init the Game Screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    #Adding the player and initiating groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updatable,drawable)
-    Asteroid.containers = (asteroids,updatable,drawable)
-    AsteroidField.containers = (updatable)
-    player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-    asteroidfield = AsteroidField()
-    
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        # Draw and update the player
-        for object in updatable:
-            object.update(dt)
-        # Check for collisions
+
+        for obj in updatable:
+            obj.update(dt)
+
         for asteroid in asteroids:
-            if(asteroid.check_collision(player1)):
-                print("Game Over!")
+            if asteroid.collides_with(player):
+                print("Game over!")
                 sys.exit()
 
-        # Clear the screen
         screen.fill("black")
 
-        # Add drawable objects
-        for object in drawable:
-            object.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
-        # Refresh the display
         pygame.display.flip()
 
-        # Limit the FPS to 60
-        dt = gametime.tick(60) / 1000
-        #DB print(dt)
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
